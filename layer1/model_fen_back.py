66920a877d33b1ceb8a53a49318cefa4cc217368
Johnny Bravo <arpitkumar1412@gmail.com>
2021-05-23 02:08:53 +0530

    cost vol updated

--- layer1/model_fen_back.py
+++ layer1/model_fen_back.py
@@ -50,36 +50,6 @@ def feature_extractor_and_layer_flow_estimator():
   model.add(tf.keras.layers.Reshape((6, final_width, final_height, final_channels)))
   return model
 
-# def cal_cost(features):
-#   print("calculating cost")
-#   cost = np.zeros((1000, 6, width, height, 3))
-#   for i in range(features.shape[0]):
-#     for j in range(6):
-#       flat_feature1 = mixed[i,j,:,:,0]
-#       flat_feature2 = mixed[i,j+1,:,:,0]
-#       cost_vol = scipy.signal.correlate2d(flat_feature1, flat_feature2, mode='same')   #normalize here ############################
-#       img = np.reshape(cost_vol, (width, height))
-#       cost[i,j,:,:,0] = img
-#
-#       flat_feature1 = mixed[i,j,:,:,1]
-#       flat_feature2 = mixed[i,j+1,:,:,1]
-#       cost_vol = scipy.signal.correlate2d(flat_feature1, flat_feature2, mode='same')   #normalize here ############################
-#       img = np.reshape(cost_vol, (width, height))
-#       cost[i,j,:,:,0] = img
-#
-#       flat_feature1 = mixed[i,j,:,:,2]
-#       flat_feature2 = mixed[i,j+1,:,:,2]
-#       cost_vol = scipy.signal.correlate2d(flat_feature1, flat_feature2, mode='same')   #normalize here ############################
-#       img = np.reshape(cost_vol, (width, height))
-#       cost[i,j,:,:,0] = img
-#     print(i)
-#   inp = np.zeros((1000, 6, 3*width, height, 3))
-#   for i in range(1000):
-#     for j in range(6):
-#       inp[i][j] = tf.concat([mixed[i][j], mixed[i][j+1], cost[i][j]], axis=0)
-#   return tf.convert_to_tensor(inp)
-# inp = cal_cost(mixed)
-# np.save('reflection-inp.npy', inp)
 back = feature_extractor_and_layer_flow_estimator()
 back.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 X_train, X_test, y_train, y_test = train_test_split(inp, vid1, test_size=0.2, random_state=42)
