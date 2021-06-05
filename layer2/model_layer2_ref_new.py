7581c17c3b837b391c1ef44f8470ea9ae6d13bb1
Johnny Bravo <arpitkumar1412@gmail.com>
2021-06-05 17:16:09 +0530

    gpu

--- layer2/model_layer2_ref_new.py
+++ layer2/model_layer2_ref_new.py
@@ -30,11 +30,11 @@ os.environ["CUDA_VISIBLE_DEVICES"]='0,1'
 
 import torch
 
-torch.cuda.is_available()
-torch.cuda.current_device()
-torch.cuda.device(0)
-torch.cuda.device_count()
-torch.cuda.get_device_name(0)
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
 
 
 def convert_pred(vid, shape):
