654a40686b69d5e94352b201bd5f9db0b6a2646a
Arpit Kumar <arpitkumar1412@gmail.com>
2021-09-01 21:43:31 +0530

    local changes

--- testing.py
+++ testing.py
@@ -116,11 +116,11 @@ mixed = np.load('../data/reflection-mixed.npy')
 inp = np.load('../data/reflection-inp.npy')
 print("data loaded")
 
-pred_layer1_back = back.predict(tf.expand_dims(inp[i], axis=0))
-pred_layer1_obs = obs.predict(tf.expand_dims(inp[i], axis=0))
-print(pred_layer1_back.shape)
-pred_layer1_back = tf.squeeze(pred_layer1_back)[0]
-array_to_img(pred_layer1_back).save('prediction_layer1_1.png')
+#pred_layer1_back = back.predict(tf.expand_dims(inp[i], axis=0))
+#pred_layer1_obs = obs.predict(tf.expand_dims(inp[i], axis=0))
+#print(pred_layer1_back.shape)
+#pred_layer1_back = tf.squeeze(pred_layer1_back)[0]
+#array_to_img(pred_layer1_back).save('prediction_layer1_1.png')
 # array_to_img(pred_layer1_obs).save('prediction_layer1_2.png')
 
 TORCH_R2PLUS1D = "moabitcoin/ig65m-pytorch"  # From https://github.com/moabitcoin/ig65m-pytorch
