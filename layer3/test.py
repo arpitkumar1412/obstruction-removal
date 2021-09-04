87ecb640a52b467fd3a87e40ca9e431f3596de4b
arpitkumar1412 <Arpitsahil10685>
2021-09-04 20:45:18 +0530

    edits

---
+++ layer3/test.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import numpy as np
+from numpy import img_to_array
+
+file = '../layer2_prediction/back_ref/back_ref0.png'
+img = Image.open(file).resize((256,256*6)).save('test.png')
