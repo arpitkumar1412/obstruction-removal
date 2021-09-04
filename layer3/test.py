91bdddce99f316a7ab0f8263587cedcc565b0023
arpitkumar1412 <Arpitsahil10685>
2021-09-04 20:48:54 +0530

    edits

--- layer3/test.py
+++ layer3/test.py
@@ -3,4 +3,8 @@ import numpy as np
 from numpy import asarray
 
 file = '../layer2_prediction/back_ref/back_ref0.png'
-img = Image.open(file).resize((256,256*6)).save('test.png')
+img = Image.open(file).resize((256,256*6))
+src = list()
+src.append(asarray(img))
+src = asarray(src)
+Image.fromarray(src[0]).save('test.png')
